cfd4762bf3a69526600dc2e3e44f0d3f358d1515
gnladu07 <gnlaud07@gmail.com>
2024-11-22 14:38:15 +0900

    clean member mapping

--- src/main/java/com/itbank/finalProject/repository/MemberDAO.java
+++ src/main/java/com/itbank/finalProject/repository/MemberDAO.java
@@ -2,6 +2,8 @@ package com.itbank.finalProject.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.itbank.finalProject.model.BookingDTO;
 import com.itbank.finalProject.model.MemberDTO;
 import com.itbank.finalProject.model.ProfileDTO;
@@ -9,48 +11,56 @@ import com.itbank.finalProject.model.SubLocationDTO;
 
 public interface MemberDAO {
 
-   int naverInsert(MemberDTO dto);
+	int naverInsert(MemberDTO dto);
 
-   String selectSalt(String userid);
+	String selectSalt(String userid);
 
-   MemberDTO selectLogin(MemberDTO dto);
+	MemberDTO selectLogin(MemberDTO dto);
 
-   MemberDTO selectNaverLogin(String naver_id);
+	MemberDTO selectNaverLogin(String naver_id);
 
-   int selectCount(String userid);
+	int selectCount(String userid);
 
-   int selectEmailAndUserid(MemberDTO dto);
+	int selectEmailAndUserid(MemberDTO dto);
 
-   int updatePassword(MemberDTO dto);
+	int updatePassword(MemberDTO dto);
 
-   MemberDTO getMemberByeId(int id);
+	MemberDTO getMemberByeId(int id);
 
-   int insertProfile_img(ProfileDTO profile);
+	int insertProfile_img(ProfileDTO profile);
 
-   int selectMaxProfileId();
+	int selectMaxProfileId();
 
-   int updateMemberProfile(MemberDTO dto);
+	int updateMemberProfile(MemberDTO dto);
 
-   int updateLocation(MemberDTO dto);
+	int updateLocation(@Param("location") String location, @Param("id") int id);
 
-   MemberDTO selectUserById(int id);
+	MemberDTO selectUserById(int id);
 
-   int updatePw(MemberDTO userInfo);
+	int updatePw(MemberDTO userInfo);
 
-   MemberDTO checkByUserId(String email);
+	MemberDTO checkByUserId(String email);
 
-   MemberDTO checkByEmail(String userid);
+	MemberDTO checkByEmail(String userid);
 
-   List<BookingDTO> selectMemberByBookingList(int id);
+	List<BookingDTO> selectMemberByBookingList(int id);
 
-   int insertAddLocation(SubLocationDTO dto);
+	int insertAddLocation(SubLocationDTO dto);
 
-   List<SubLocationDTO> selectAddLocationList(int id);
+	int checkDuplicateLocation(SubLocationDTO dto);
+	
+	List<SubLocationDTO> selectAddLocationList(int id);
 
-   int deleteAddLocation(int id);
+	int deleteAddLocation(int id);
 
-//   MemberDTO selectUserByEmail(String email);
+	MemberDTO selectOne(int id);
 
+	int getMaxId();
 
+	int insertsubLocation(SubLocationDTO subDTO);
+
+	int updateSubLocation(SubLocationDTO dto);
+
+	MemberDTO selectOneBySubLocation(int id);
 
 }
